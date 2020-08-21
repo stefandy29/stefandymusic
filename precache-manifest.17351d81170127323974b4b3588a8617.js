79acda66a8b97fca80f4bb09fd20a6c61bb984f4
stefandy29 <55833817+stefandy29@users.noreply.github.com>
2020-08-21 12:27:57 +0700

    Updates

---
+++ precache-manifest.17351d81170127323974b4b3588a8617.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26053949a8a1d20ca96e18f03ef2367c",
+    "url": "/stefandymusic/index.html"
+  },
+  {
+    "revision": "82e864917f484e46c819",
+    "url": "/stefandymusic/static/css/main.5cfa6024.chunk.css"
+  },
+  {
+    "revision": "1c876a78b27dacea71a7",
+    "url": "/stefandymusic/static/js/2.0a95a209.chunk.js"
+  },
+  {
+    "revision": "40158217dcba29c41bf6c1afe3c208d6",
+    "url": "/stefandymusic/static/js/2.0a95a209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e864917f484e46c819",
+    "url": "/stefandymusic/static/js/main.57c11ab1.chunk.js"
+  },
+  {
+    "revision": "6cb5e3a05b61a8685b41",
+    "url": "/stefandymusic/static/js/runtime-main.afe34ea7.js"
+  },
+  {
+    "revision": "eb7f4d35290ffbc3c2711e165eb539e4",
+    "url": "/stefandymusic/static/media/bc.eb7f4d35.png"
+  },
+  {
+    "revision": "f7fdd446b27dff66de77e2910a517263",
+    "url": "/stefandymusic/static/media/coverphoto.f7fdd446.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stefandymusic/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c0baf485f04520f4e02e478ce8a0d065",
+    "url": "/stefandymusic/static/media/yb.c0baf485.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stefandy29/stefandymusic/precache-manifest.0fd8b6d09d4e3d9bdb7f801b35da3161.js"
+  "/stefandymusic/precache-manifest.17351d81170127323974b4b3588a8617.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stefandy29/stefandymusic/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stefandymusic/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
