309b1721847169c82dd05c62477c15e296a9ce7f
stefandy29 <55833817+stefandy29@users.noreply.github.com>
2020-08-21 12:21:08 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/stefandy29/react-gh-pages/precache-manifest.8a45299510ccdfeb54cea05323a69df5.js"
+  "/stefandy29/stefandymusic/precache-manifest.0fd8b6d09d4e3d9bdb7f801b35da3161.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stefandy29/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stefandy29/stefandymusic/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
