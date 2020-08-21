6ddf4e45bb0ad8fdc10f684d0be68ad802364ac2
stefandy29 <55833817+stefandy29@users.noreply.github.com>
2020-08-21 12:16:53 +0700

    Updates

---
+++ precache-manifest.8a45299510ccdfeb54cea05323a69df5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb723559bc16028559476f905bea0bac",
+    "url": "/stefandy29/react-gh-pages/index.html"
+  },
+  {
+    "revision": "c135e93335d2b4167779",
+    "url": "/stefandy29/react-gh-pages/static/css/main.5cfa6024.chunk.css"
+  },
+  {
+    "revision": "1c876a78b27dacea71a7",
+    "url": "/stefandy29/react-gh-pages/static/js/2.0a95a209.chunk.js"
+  },
+  {
+    "revision": "40158217dcba29c41bf6c1afe3c208d6",
+    "url": "/stefandy29/react-gh-pages/static/js/2.0a95a209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c135e93335d2b4167779",
+    "url": "/stefandy29/react-gh-pages/static/js/main.f5bb505f.chunk.js"
+  },
+  {
+    "revision": "8204c43dd90b240989fb",
+    "url": "/stefandy29/react-gh-pages/static/js/runtime-main.5e37d486.js"
+  },
+  {
+    "revision": "eb7f4d35290ffbc3c2711e165eb539e4",
+    "url": "/stefandy29/react-gh-pages/static/media/bc.eb7f4d35.png"
+  },
+  {
+    "revision": "f7fdd446b27dff66de77e2910a517263",
+    "url": "/stefandy29/react-gh-pages/static/media/coverphoto.f7fdd446.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stefandy29/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c0baf485f04520f4e02e478ce8a0d065",
+    "url": "/stefandy29/react-gh-pages/static/media/yb.c0baf485.png"
+  }
+]);
